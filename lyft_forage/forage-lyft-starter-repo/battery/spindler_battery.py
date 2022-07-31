b0e1d660ae106956d199981f139c86336acaf90d
Siddharth Saraswat <73304623+siddharth1012@users.noreply.github.com>
2022-07-31 15:23:21 +0530

    Add files via upload

---
+++ lyft_forage/forage-lyft-starter-repo/battery/spindler_battery.py
@@ -0,0 +1,14 @@
+from battery.battery import Battery
+from utils import add_years_to_date
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+        
+    def needs_service(self):
+        date_battery_serviced = add_years_to_date(self.last_service_date, 2) 
+        if(date_battery_serviced<self.current_date):
+            return True
+        else:
+            return False
